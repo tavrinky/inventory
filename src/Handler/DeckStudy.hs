8853da32e160806d67a77f76335f0b1268c6206f
tavrinky <jackbrandt11@gmail.com>
2018-11-13 20:47:34 -0600

    Deleted decks, updated rather than inserted in deckstudy

--- src/Handler/DeckStudy.hs
+++ src/Handler/DeckStudy.hs
@@ -7,14 +7,14 @@ module Handler.DeckStudy where
 import Import
 import Utils 
 import Data.Maybe
+import Data.Serialize
 
 getDeckStudyR :: DeckId -> Handler Html
 getDeckStudyR deckId = do 
     
   (Deck title contents _) <- runDB $ get404 deckId 
   let cards = getCards contents 
-  let cols = map (\(Card term def _ _) -> (decodeUtf8 term, decodeUtf8 def)) cards
-  --_ <- runDB $ deleteWhere [TableTablecontents !=. ""]
+  --_ <- runDB $ deleteWhere [DeckTitle !=. ""]
   defaultLayout $ do
       $(widgetFile "deckstudy")
 
@@ -27,7 +27,13 @@ postDeckStudyR deckId = do
       readCards = fromJust $ readMay preCards 
       cards = map mkCard readCards 
   
-  defaultLayout $ toWidget [hamlet| #{show cards} |]
+  runDB $ update deckId [DeckCards =. (runPut . put $ cards)]
+
+
+  
+  defaultLayout $ toWidget [hamlet| #{show cards} |] 
+
+
 
   where 
     mkCard (term:def:correct:incorrect:[]) = Card (encodeUtf8 term) (encodeUtf8 def) (fromJust $ readMay correct) (fromJust $ readMay incorrect) 
